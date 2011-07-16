d168f960819a7aa1225fa0c08902fe112ae8efd6
Fredrik Strandin <fredrik.strandin@gmail.com>
2011-07-16 20:22:53 +0200

    Default Eclipse-project to work from

---
+++ src/se/kd35a/blekingskaSangbok/BlekingskaSangbokActivity.java
@@ -0,0 +1,13 @@
+package se.kd35a.blekingskaSangbok;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlekingskaSangbokActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
