febfe35ce3d6d56314c3fb2964a749df92abfcc4
Fredrik Strandin <fredrik.strandin@gmail.com>
2011-07-16 21:41:49 +0200

    A tabbed layout to start from. The tabs aren't filled with anything yet. References #1

--- gen/se/kd35a/blekingskaSangbok/R.java
+++ gen/se/kd35a/blekingskaSangbok/R.java
@@ -14,10 +14,17 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int list=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int search=0x7f030003;
+        public static final int start=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int about=0x7f040004;
+        public static final int app_name=0x7f040000;
+        public static final int list=0x7f040003;
+        public static final int search=0x7f040002;
+        public static final int start=0x7f040001;
     }
 }
--- src/se/kd35a/blekingskaSangbok/BlekingskaSangbokActivity.java
+++ src/se/kd35a/blekingskaSangbok/BlekingskaSangbokActivity.java
@@ -1,13 +1,43 @@
 package se.kd35a.blekingskaSangbok;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class BlekingskaSangbokActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class BlekingskaSangbokActivity extends TabActivity {
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		TabHost tabHost = getTabHost();
+		TabHost.TabSpec spec;
+		Intent intent;
+		
+		intent = new Intent().setClass(this, StartActivity.class);
+		spec = tabHost.newTabSpec("start").setIndicator("Start")
+						.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, SearchActivity.class);
+		spec = tabHost.newTabSpec("search").setIndicator("Sök")
+						.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, ListActivity.class);
+		spec = tabHost.newTabSpec("list").setIndicator("Lista")
+						.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, AboutActivity.class);
+		spec = tabHost.newTabSpec("about").setIndicator("Om")
+						.setContent(intent);
+		tabHost.addTab(spec);
+		
+		tabHost.setCurrentTab(0);
+	}
+
 }
